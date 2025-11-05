5853cb6a0ee7aa8568be39a94ebd8a9bcced3c53
jillianyeow18 <jillian.yeow@gmail.com>
2025-11-05 11:07:21 +0800

    chore: update types to remove admin role from enum

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -373,7 +373,7 @@ export type CompositeTypes<
 export const Constants = {
   public: {
     Enums: {
-      app_role: ["admin", "staff", "student"],
+      app_role: ["staff", "student"],
     },
   },
 } as const;
